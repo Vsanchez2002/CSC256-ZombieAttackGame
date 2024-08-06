fc1c2899f4377a84e7367986c050acf79c5179c6
Vsanchez2002 <119705686+Vsanchez2002@users.noreply.github.com>
2024-08-06 15:16:45 -0700

    Update ZombieHelpPI.s

--- ZombieHelpPI.s
+++ ZombieHelpPI.s
@@ -2,7 +2,7 @@
 
 	.text
 	.align	2
-	.global	updateZombies
+	.global	updateZombies 
 	.type	updateZombies, %function
 updateZombies:
 .LFB1760:
@@ -15,9 +15,27 @@ updateZombies:
 	str	x19, [sp, 16]
 	.cfi_offset 19, -48
 
+ 	// Initialize the loop counter
+	mov	x19, x2        // x19 = number of zombies
+	mov	x20, x0        // x20 = pointer to zombie array
 
+	loop_start:
+	cbz	x19, loop_end 
 
+ 	ldr	x0, [x20]      // x0 = *zombie_position
+	mov	x1, x1         // x1 = human_position
 
+ 	bl	moveZombie
+
+   	// Store the updated zombie position
+	str	x0, [x20]
+ 	
+
+	// Move to the next zombie
+	add	x20, x20, #8   
+	sub	x19, x19, #1
+
+	b	loop_start
 
 
 
@@ -41,6 +59,49 @@ moveZombie:
 	.cfi_startproc
 	sub	sp, sp, #32
 	.cfi_def_cfa_offset 32
+ 
+ 	sub x2,x0,x1   //temp = z - h 
+  	cmp x2, 10     // temp >= 10
+   	bge move_by_10 // if temp>=10 branch move_by_10
+
+ 	cmp x2, 0
+  	bgt move_by_1
+
+   	cmp x2, 0
+    	beq no_move
+
+      	cmp x2, -10
+	blt move_by_minus_10
+
+ 	
+    	move_by_10:
+     	sub x0,x0, 10
+      	b end_move
+
+     	move_by_1:
+      	sub x0,x0, 1
+       	b end_move
+	 
+  	move_by_minus_1:
+   	add x0,x0,1 
+	b end_move
+
+ 	move_by_minus_10:
+  	add x0,x0, 10
+  	b end_move
+   
+      	no_move:
+
+       	end_move:
+       	
+
+       
+      
+     
+    
+    	
+  	
+  	
 
 
 
@@ -52,5 +113,3 @@ moveZombie:
 	.cfi_endproc
 .LFE1761:
 	.size	moveZombie, .-moveZombie
-
-
